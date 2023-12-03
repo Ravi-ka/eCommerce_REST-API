a0ee8a1a1d823df93740b83c999172eda5dca78c
Ravisankar Ka <ka.ravisankar@gmail.com>
2023-12-03 22:28:22 +0530

    Logging using npm Winston

--- src/middlewares/logger.middleware.js
+++ src/middlewares/logger.middleware.js
@@ -1,22 +1,30 @@
 import fs from "fs";
+import winston from "winston";
 
-const fsPromise = fs.promises;
+// const fsPromise = fs.promises;
 
-async function log(logData) {
-  try {
-    logData = new Date().toString() + ". Log Data : " + logData;
-    await fsPromise.appendFile("logFile.txt", logData);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// async function log(logData) {
+//   try {
+//     logData = new Date().toString() + ". Log Data : " + logData;
+//     await fsPromise.appendFile("logFile.txt", logData);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "request-logging" },
+  transports: [new winston.transports.File({ filename: "logFile.txt" })],
+});
 
 const loggerMiddleware = async (req, res, next) => {
   if (!req.url.includes("signin")) {
     const logData = `${req.url} - ${JSON.stringify(req.body)} - ${
       req.method
-    }\n\n`;
-    await log(logData);
+    } - `;
+    logger.info(logData);
   }
   next();
 };
